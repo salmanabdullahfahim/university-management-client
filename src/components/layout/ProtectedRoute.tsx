e427070472f42ed29d9e89c3cf7a3015e432fadf
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-07-06 11:42:04 +0600

    fix

--- src/components/layout/ProtectedRoute.tsx
+++ src/components/layout/ProtectedRoute.tsx
@@ -7,7 +7,7 @@ const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const token = useAppSelector(useCurrentToken);
 
   if (!token) {
-    <Navigate to="/login" replace={true} />;
+    return <Navigate to="/login" replace={true} />;
   }
 
   return children;
